fcbc583d9598fdc5eda5d9bc392f6628a296d1db
0xGLSS <85647797+0xGLSS@users.noreply.github.com>
2023-07-04 23:29:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Check Server Status</title>
-</head>
-<body>
-    <h2>Check if server is up</h2>
-    <form>
-        <input type="text" id="ipInput" placeholder="Enter server IP"/>
-        <input type="button" value="Check status" onclick="checkStatus()"/>
-    </form>
+<b>Check if server is up</b>
+<form method="POST">
+    <input type="text" name="ip" placeholder="Enter server IP"/>
+    <input type="submit" value="check status"/>
+</form>
 
-    <div id="result"></div>
+<?php
+#Payload: 127.0.0.1;cat /etc/passwd
 
-    <script>
-        function checkStatus() {
-            var ip = document.getElementById('ipInput').value;
-            var xhr = new XMLHttpRequest();
-            
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4) {
-                    if (xhr.status === 0) {
-                        document.getElementById('result').innerHTML = 'Error: Failed to send the request.';
-                    } else {
-                        document.getElementById('result').innerHTML = xhr.responseText;
-                    }
-                }
-            };
-            
-            xhr.open('POST', 'https://api.example.com/check-status', true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.send('ip=' + encodeURIComponent(ip));
-        }
-    </script>
-</body>
-</html>
+if(isset($_POST['ip'])){
+    #Insecure Implementation
+    echo passthru('ping -c 1 '.$_POST['ip']);
+
+    /*Secure Implementation
+    if ($_POST['ip'] = filter_input(INPUT_POST,'ip',FILTER_VALIDATE_IP)) {
+        echo passthru('ping -c 1 '.$_POST['ip']);
+    }else {
+        die("Please provide a valid IP address");
+    }*/
+}
+
+#Notes
+#system() => Execute an external program and displays the output.
+#passthru() => Same than system, but casts the output in binary "as is" from the shell to the PHP output (typically the HTTP response).
+#exec() => Captures the output and only the last line of the output into a string.
+#shell_exec() => Same than exec, but capturing full output, not only the last line.
+
+#Preventio: By far the most effective way to prevent OS command injection vulnerabilities is to never call out to OS commands from application-layer code. In virtually every case, there are alternate ways of implementing the required functionality using safer platform APIs
+?>
